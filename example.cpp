08b53bdacdc996fe58b325a8397605b561d84134
f3fe-hash <f3fe5.unhash@gmail.com>
2025-02-13 16:31:51 -0500

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -23,15 +23,20 @@ vector<vector<double>> y = {
 
 int main()
 {
+    train_settings.max_cores        = 4;     // Edit as needed
+    train_Settings.neuralnetworks   = 10000; // 10,000
+    train_settings.rounds           = 100;   // 100
+    train_settings.epochs_per_round = 10;    // 10
+    train_settings.max_drop         = 0.01;
+
     try
     {
         train_settings.rounds = 10;
 
         // Create the network
-        NeuralNetwork nn(AI_settings);
+        AI nn(AI_settings);
         nn.addLayer(2, "tanh");
-        nn.addLayer(10, "tanh");
-        nn.addLayer(10, "tanh");
+        nn.addLayer(5, "tanh");
         nn.addLayer(1, "tanh");
 
         dataset_t dataset = createDataset(X, y, 0.5, 0.5);
@@ -39,7 +44,7 @@ int main()
         // Start timer before the training loop
         auto start = chrono::high_resolution_clock::now();
 
-        nn.run_round(100000, dataset);  // Run the network training
+        nn.train(train_settings, dataset);  // Run the network training
 
         // End timer after the training loop
         auto end = chrono::high_resolution_clock::now();
@@ -48,9 +53,7 @@ int main()
         chrono::duration<double> duration = end - start;
 
         // Test the AI
-        double score = 0.00;
-        for (size_t i = 0; i < dataset.test_size; i++)
-            score = (score + nn.calc_abs_error(nn.forward(X[0]), y[0])) / 2;
+        double score = nn.test(dataset);
 
         // Print the execution time and score
         cout << "\nExecution time: " << duration.count() << " seconds\n";
